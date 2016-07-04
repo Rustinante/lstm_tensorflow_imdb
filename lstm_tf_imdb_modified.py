c00ec70e10afbd2452bb97f0845953bc349a8af9
Aaron Zhou Qian <aaronzhouqian@ucla.edu>
2016-07-03 19:18:33 -0700

    trying to fix GPU usage

--- lstm_tf_imdb_modified.py
+++ lstm_tf_imdb_modified.py
@@ -241,7 +241,6 @@ def run_epoch(session, m, data, is_training, verbose=True):
                                                                 m._targets: labels_mini,
                                                                 m._mask: mask})
             total_num_correct_predictions+= num_correct_predictions
-            print(type(total_num_correct_predictions))
 
         avg_accuracy = total_num_correct_predictions/num_samples_seen
         print("Traversed through %d samples." %num_samples_seen)
