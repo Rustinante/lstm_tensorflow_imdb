8886b809f1be8c185dae97cd427128ac0b8e3bfe
Aaron Zhou Qian <aaronzhouqian@ucla.edu>
2016-07-04 00:06:49 -0700

    random mini-batch

--- lstm_tf_imdb.py
+++ lstm_tf_imdb.py
@@ -218,7 +218,7 @@ def run_epoch(session, m, data, is_training, verbose=True):
     labels=[]
     for l in list_of_training_index_list:
         x.append([data[0][i] for i in l])
-        labels.append((data[1][i] for i in l))
+        labels.append([data[1][i] for i in l])
 
     if is_training:
         if flags.first_training_epoch:
