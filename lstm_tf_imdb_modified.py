ebf383827444b60a8fe27013cd8b9a5930a3ca8b
Aaron Zhou Qian <aaronzhouqian@ucla.edu>
2016-07-06 09:19:01 -0700

    restructuring the cell

--- lstm_tf_imdb_modified.py
+++ lstm_tf_imdb_modified.py
@@ -30,7 +30,7 @@ ACCURACY_THREASHOLD= 0.90
 np.random.seed(13)
 
 class Options(object):
-    DATA_MAXLEN = 200
+    DATA_MAXLEN = 100
     CELL_MAXLEN = 100
     VALIDATION_PORTION = 0.05
     patience = 10
